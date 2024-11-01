b259517f56bb2296a1a55ac9a3af0f771cbf5fd0
tasleem-m <trmuhammad2017@gmail.com>
2024-10-31 20:02:41 -0500

    post class review

--- Scripts/baseball script.sql
+++ Scripts/baseball script.sql
@@ -93,6 +93,8 @@ INNER JOIN people AS p
 USING (playerid)
 WHERE (sb + cs) >= 20 AND yearid = 2016
 ORDER BY pct_successful_steals DESC
+--answer: Chris Owings (Christopher Scott) at 91%
+--50 rows; SUM sb and cs to account for all players from all teams
 
 --q7 From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
 SELECT teamid, yearid, g, MAX (w) AS largest_wins
@@ -101,6 +103,7 @@ WHERE wswin = 'N'
 	AND yearid BETWEEN 1970 AND 2016
 GROUP BY teamid, yearid, g
 ORDER BY largest_wins DESC
+--answer: 116 from SEA
 
 SELECT teamid, yearid, g, MIN (w) AS smallest_wins
 FROM teams
@@ -108,6 +111,7 @@ WHERE wswin = 'Y'
 	AND yearid BETWEEN 1970 AND 2016
 GROUP BY teamid, yearid, g
 ORDER BY smallest_wins DESC
+--answer: 114 from NYA
 
 --The 1981 strike resulted in regular season games being cancelled. 
 
@@ -118,6 +122,7 @@ WHERE wswin = 'Y'
 	AND yearid <> 1981
 GROUP BY teamid, yearid, g
 ORDER BY smallest_wins DESC
+--1981 excluded
 
 WITH winners AS (SELECT yearid, wswin, MAX (w) AS largest_wins 
 					FROM teams 
@@ -147,24 +152,28 @@ pct AS (SELECT SUM (CASE WHEN allw.winlarge >= allw.loselarge THEN 1
 
 SELECT ROUND((total_wins::numeric/cnt_year::numeric), 2) * 100 AS pct_time
 FROM pct
+--answer:27%
+--25.53% with 12/47 (1981 and 1994 strikes)
 		
 --q8 Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
-SELECT name, park, attendance/ghome AS avg_attendance
+(SELECT name, park, attendance/ghome AS avg_attendance
 FROM teams AS t
 WHERE yearid = '2016'
 AND ghome >= 10
 ORDER BY avg_attendance DESC
-LIMIT 5
+LIMIT 5)
 
-SELECT name, park, attendance/ghome AS avg_attendance
+UNION ALL
+
+(SELECT name, park, attendance/ghome AS avg_attendance
 FROM teams AS t
 WHERE yearid = '2016'
 AND ghome >= 10
 ORDER BY avg_attendance
-LIMIT 5
+LIMIT 5)
 
 --q9 Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
-SELECT playerid, tsnwinners.lgid, tsnwinners.yearid, m.teamid, t.name, p.namefirst, p.namelast
+SELECT tsnwinners.playerid, tsnwinners.lgid, tsnwinners.yearid, m.teamid, t.name, p.namefirst, p.namelast
 FROM 			(SELECT playerid, lgid, yearid
 				FROM awardsmanagers
 				WHERE awardid = 'TSN Manager of the Year'
@@ -180,14 +189,15 @@ FROM 			(SELECT playerid, lgid, yearid
 						WHERE awardid = 'TSN Manager of the Year'
 						AND lgid = 'AL')) AS tsnwinners
 INNER JOIN managers AS m  --to get team id
-USING (playerid)
+ON tsnwinners.playerid=m.playerid AND tsnwinners.yearid=m.yearid
 INNER JOIN teams AS t --to get team name
-USING (teamid)
+ON m.teamid=t.teamid AND t.yearid=m.yearid
 INNER JOIN people AS p --to get full name
-USING (playerid)
-WHERE m.yearid = tsnwinners.yearid
-	AND m.yearid = t.yearid
+ON tsnwinners.playerid=p.playerid
+--WHERE m.yearid = tsnwinners.yearid
+	--AND m.yearid = t.yearid
 ORDER BY playerid
+--answer: Davey Johnson and Jim Leyland
 
 --q10 Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
 SELECT DISTINCT p.namefirst, p.namelast, players.max_hr
@@ -213,8 +223,3 @@ ON b.playerid=p.playerid
 WHERE yearid = 2016
 	AND max_hr > 0
 ORDER BY max_hr DESC
-
-
-SELECT playerid, yearid, hr
-FROM batting
-WHERE playerid = 
